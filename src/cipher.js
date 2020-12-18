1daf3a6941cd7dff52e153c87106d9c113403e63
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2020-12-18 13:24:52 -0600

    Actualizaciones al 18 de diciembre 13:24. Código funcionando y diseño implementándose

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,82 @@
-const cipher = {
-  // ...
-};
-
-export default cipher;
+const cipher = { // object initializer
+  //objetc´s methods - encode, decode
+  encode: function cifrar(desplazamiento, password){ //objects properties - parámetros de la función dentro del método.
+   const mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+   const minusculas = "abcdefghijklmnopqrstuvwxyz";
+   const numeros = "0123456789";
+   const especiales = "!\"#$%&'()*+,-./"
+   let passwordCifrada = "";
+   if (password == []) {
+     throw new TypeError ("Contraseña inválida!");
+   } else if (desplazamiento == null){
+     throw new TypeError ("Campo de número vacío!");
+   } else if (password == 0 && desplazamiento == 0){
+     throw new TypeError ("Los campos no son válidos!");
+   }
+   
+   for(var i = 0; i <= (password.length-1); i++){
+     let ascii = password.charCodeAt(i);
+         if (mayusculas.includes(password[i])){
+             let formula = (ascii + desplazamiento - 65) % 26 + 65;
+             let abecedario = String.fromCharCode(formula);
+             passwordCifrada += abecedario;
+         } else if (minusculas.includes(password[i])){
+             let formula = (ascii + desplazamiento - 97) % 26 + 97;
+             let abecedario = String.fromCharCode(formula);
+             passwordCifrada += abecedario;
+         } else if (numeros.includes(password[i])){
+             let formula = (ascii + desplazamiento - 48) % 26 + 48;
+             let abecedario = String.fromCharCode(formula);
+             passwordCifrada += abecedario;
+         } else if (especiales.includes(password[i])){
+             let formula = (ascii + desplazamiento - 33) % 26 + 33;
+             let abecedario = String.fromCharCode(formula);
+             passwordCifrada += abecedario;
+         } else {
+             passwordCifrada += password[i];
+         }
+   }
+   return(passwordCifrada);
+   },
+   
+   decode: function (desplazamiento, password){
+     const mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+     const minusculas = "abcdefghijklmnopqrstuvwxyz";
+     const numeros = "0123456789";
+     const especiales = "!\"#$%&'()*+,-./"
+     let passwordDescifrada = "";
+     if (password == []) {
+       throw new TypeError ("Contraseña inválida!");
+     } else if (desplazamiento == null){
+       throw new TypeError ("Campo de número vacío!");
+     } else if (password == 0 && desplazamiento == 0){
+       throw new TypeError ("Los campos no son válidos!");
+     }
+    
+   
+     for(var i = 0; i <= (password.length-1); i++){
+       let ascii = password.charCodeAt(i);
+         if (mayusculas.includes(password[i])){
+           let formula = (ascii - desplazamiento - 90) % 26 + 90;
+           let abecedario = String.fromCharCode(formula);
+           passwordDescifrada += abecedario;
+         } else if (minusculas.includes(password[i])){
+           let formula = (ascii - desplazamiento - 122) % 26 + 122;
+           let abecedario = String.fromCharCode(formula);
+           passwordDescifrada += abecedario;
+         } else if (numeros.includes(password[i])){
+           let formula = (ascii - desplazamiento - 57) % 26 + 57;
+           let abecedario = String.fromCharCode(formula);
+           passwordDescifrada += abecedario;
+         } else if (especiales.includes(password[i])){
+           let formula = (ascii - desplazamiento - 47) % 26 + 47;
+           let abecedario = String.fromCharCode(formula);
+           passwordDescifrada += abecedario;
+         } else {
+           passwordDescifrada += password[i];
+           }
+       }
+     return(passwordDescifrada);
+       }
+     } 
+   export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import cipher from './cipher.js';
-
-console.log(cipher);
+ 
+let password = document.getElementById("password").value;
+let desplazamiento = parseInt(document.getElementById("desplazamiento").value);// para convertir estos valores a número
+let boton = document.getElementById("cifrar");
+let boton2 = document.getElementById("descifrar");
+ 
+boton.addEventListener("click", function(){
+ if (password.length == 0 || isNaN(desplazamiento)) {
+   document.getElementById("password_cifrada").innerText = "¡NO HAY NADA!";
+ } else {
+   let passwordCifrada = cipher.encode(desplazamiento, password);
+   document.getElementById("password_cifrada").innerText = passwordCifrada;
+   //document.getElementById("password_cifrada").innerText = texto; 
+ }
+}, true)
+ 
+boton2.addEventListener("click", function(){
+ if (password.length == 0 || isNaN(desplazamiento)) {
+   document.getElementById("password_cifrada").innerText = "¡NO HAY NADA!";
+ } else {
+   let passwordCifrada = cipher.decode(desplazamiento, password);
+   document.getElementById("password_cifrada").innerText = passwordCifrada;
+   //document.getElementById("password_cifrada").innerText = texto; 
+ }
+}, true)
