3fdc1e628d5ac35107031d13c0a57f4af4e2febc
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2021-01-03 15:20:43 -0600

    Diseño de app PASSgenerator terminado para entrega de tercer sprint

--- src/index.js
+++ src/index.js
@@ -4,23 +4,46 @@ let password = document.getElementById("password").value;
 let desplazamiento = parseInt(document.getElementById("desplazamiento").value);// para convertir estos valores a número
 let boton = document.getElementById("cifrar");
 let boton2 = document.getElementById("descifrar");
- 
+
+
+//INCIO
+/*boton.addEventListener("click", function(){
+  if (boton == false){
+    document.getElementById("password_cifrada").style.display="none";
+  }
+  else {
+    document.getElementById("password_cifrada").style.display="block";
+  }
+}, true)*/
 boton.addEventListener("click", function(){
- if (password.length == 0 || isNaN(desplazamiento)) {
-   document.getElementById("password_cifrada").innerText = "¡NO HAY NADA!";
+  document.getElementById("password_cifrada").hidden = true;
+  document.getElementById("campo_vacio").hidden= false;
+}, true);
+
+boton.addEventListener("click", function(){
+  if (password.length == 0 || isNaN(desplazamiento)) {
+   document.getElementById("password_cifrada").innerText = "Los datos que ingresaste no son válidos.";
  } else {
    let passwordCifrada = cipher.encode(desplazamiento, password);
-   document.getElementById("password_cifrada").innerText = passwordCifrada;
+   document.getElementById("password_cifrada").innerText = "Aquí tienes tu contraseña segura:\n" + "\n" + passwordCifrada;
    //document.getElementById("password_cifrada").innerText = texto; 
  }
 }, true)
  
 boton2.addEventListener("click", function(){
  if (password.length == 0 || isNaN(desplazamiento)) {
-   document.getElementById("password_cifrada").innerText = "¡NO HAY NADA!";
+   document.getElementById("password_cifrada").innerText = "Los datos que ingresaste no son válidos.";
  } else {
    let passwordCifrada = cipher.decode(desplazamiento, password);
    document.getElementById("password_cifrada").innerText = passwordCifrada;
    //document.getElementById("password_cifrada").innerText = texto; 
  }
 }, true)
+
+
+// Limpiar valores
+//document.getElementById("clear"), () => {
+  //document.getElementById("password").value="";
+  //document.getElementById("desplazamiento").value="";
+  //document.getElementById("password_cifrada").value="";
+//}
